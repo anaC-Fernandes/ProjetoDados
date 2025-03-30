49bd8df0f81ba553fe2b44f875af836ce5b637cc
anaC-Fernandes <ana.clementino@ufv.br>
2025-03-30 15:36:36 -0300

    Add script para juntar notebooks e notebooks crise juntos

---
+++ merge_notebook.py
@@ -0,0 +1,19 @@
+import nbformat
+
+# Lista de notebooks que serão mesclados
+notebooks = ["Dataset_Crise.ipynb", "Analise-Covid.ipynb"]
+output_notebook = "notebook_final.ipynb"
+
+# Criar um novo notebook vazio
+merged_notebook = nbformat.v4.new_notebook()
+
+for nb_file in notebooks:
+    with open(nb_file, "r", encoding="utf-8") as f:
+        nb = nbformat.read(f, as_version=4)
+        merged_notebook.cells.extend(nb.cells)  # Adicionar células ao notebook final
+
+# Salvar o notebook mesclado
+with open(output_notebook, "w", encoding="utf-8") as f:
+    nbformat.write(merged_notebook, f)
+
+print(f"Notebooks combinados com sucesso em {output_notebook}")
